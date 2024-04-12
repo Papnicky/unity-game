eb805ed101560a799bd045926faf32f20dcfe1a9
Nicky <kleonikh2003@gmail.com>
2024-04-12 22:13:25 +0300

    Revert "w/e"
    This reverts commit 858a30d9ce2db2871fc32a1d07207cb209ab6ce1.

--- Unity Game/Assets/PlayerControl.cs
+++ Unity Game/Assets/PlayerControl.cs
@@ -22,9 +22,14 @@ public class PlayerControl : MonoBehaviour
     {
 
 
+
         float hdirection = Input.GetAxis("Horizontal");
 
-        
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(Ground)) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            state = State.jumping;
+        }//jump
 
         if (hdirection < 0)
         {
@@ -40,12 +45,6 @@ public class PlayerControl : MonoBehaviour
         {
 
         }
-        
-        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(Ground)) 
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
-            state = State.jumping;
-        }//jump
         VelocityState();
         anim.SetInteger("state",(int) state);
     }  
